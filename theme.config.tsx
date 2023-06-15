ce72bb02cb121e4a40e84e586f7295bc18df3b32
0x0sub <sub0x1@protonmail.com>
2023-06-15 10:24:55 -0400

    navigation, first batch of docs

--- theme.config.tsx
+++ theme.config.tsx
@@ -5,15 +5,21 @@ import { BeamIcon } from "./components/Icons";
 const config: DocsThemeConfig = {
   logo: <BeamIcon />,
   project: {
-    link: "https://github.com/shuding/nextra-docs-template",
+    link: "https://github.com/Merit-Circle/beam-docs",
   },
   chat: {
     link: "https://discord.com",
   },
-  docsRepositoryBase: "https://github.com/Merit-Circle/beam-docs",
+  docsRepositoryBase: "https://github.com/Merit-Circle/beam-docs/tree/main",
   footer: {
     text: "© 2023 Merit Circle",
   },
+  sidebar: {
+    defaultMenuCollapseLevel: 1,
+  },
+  useNextSeoProps: () => ({
+    titleTemplate: "%s | Beam",
+  }),
 };
 
 export default config;
