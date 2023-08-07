47b630133a3c9825f854468d3b7484afb6c67965
Robby Uitbeijerse <robbyuitbeijerse@gmail.com>
2023-08-07 10:51:33 +0200

    chore: set fixed dependency versions

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+import nextra from 'nextra'
+
+const withNextra = nextra({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
+})
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  reactStrictMode: true
+}
+
+export default withNextra(nextConfig)
