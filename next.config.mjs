a52dbdac29b1a09416b4764f29249960e41aa733
Robby Uitbeijerse <robbyuitbeijerse@gmail.com>
2023-08-09 15:13:06 +0200

    chore: add dummy pages

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ import nextra from 'nextra'
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  staticImage: true,
+  flexsearch: {
+    codeblocks: false,
+  },
+  defaultShowCopyCode: true,
 })
 
 /**
