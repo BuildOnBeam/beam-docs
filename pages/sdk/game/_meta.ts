3a78093dad2cc30ab66e437bd28dbf28f5662584
Robby Uitbeijerse <robbyuitbeijerse@gmail.com>
2025-02-18 11:55:55 +0100

    chore: rewrite dashboard section (#204)

--- pages/sdk/game/_meta.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  introduction: 'Introduction',
-  'api-keys': 'API keys',
-  'managing-imagery': 'Managing imagery',
-  'managing-contracts': 'Managing contracts',
-  'updating-game': 'Updating game',
-};
