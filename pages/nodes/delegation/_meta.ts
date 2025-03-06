7c914b5438b4366087e682da472285b816bd1be0
Freekiebreakie <freekcaron@gmail.com>
2025-03-06 16:45:47 +0100

    chore: aanpassingen

--- pages/nodes/delegation/_meta.ts
+++ pages/nodes/delegation/_meta.ts
@@ -1,5 +1,5 @@
 export default {
-  delegate_beam: 'How to delegate BEAM',
-  delegate_node_tokens: 'How to delegate Node Tokens',
+  stake_beam: 'How to stake BEAM',
+  stake_node_tokens: 'How to stake Node Tokens',
   validate_delegate: 'Validate vs delegate'
 };
