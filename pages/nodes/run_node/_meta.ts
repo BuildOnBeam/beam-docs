bfb8364f66967442a8917fc7b9aef7a66312ab74
dubravkoMC <159017438+dubravkoMC@users.noreply.github.com>
2025-03-14 15:04:22 +0100

    Chore/separate installation methods (#222)
    * Create method-1.mdx
    
    Signed-off-by: dubravkoMC <159017438+dubravkoMC@users.noreply.github.com>
    
    * Create method-2.mdx
    
    Signed-off-by: dubravkoMC <159017438+dubravkoMC@users.noreply.github.com>
    
    * Update installation.mdx
    
    Signed-off-by: dubravkoMC <159017438+dubravkoMC@users.noreply.github.com>
    
    * Update method-1.mdx
    
    Signed-off-by: dubravkoMC <159017438+dubravkoMC@users.noreply.github.com>
    
    * Update method-1.mdx
    
    Signed-off-by: dubravkoMC <159017438+dubravkoMC@users.noreply.github.com>
    
    * Update installation.mdx
    
    Signed-off-by: dubravkoMC <159017438+dubravkoMC@users.noreply.github.com>
    
    * Update method-2.mdx
    
    Signed-off-by: dubravkoMC <159017438+dubravkoMC@users.noreply.github.com>
    
    * Update method-2.mdx
    
    Signed-off-by: dubravkoMC <159017438+dubravkoMC@users.noreply.github.com>
    
    * Update installation.mdx
    
    Signed-off-by: dubravkoMC <159017438+dubravkoMC@users.noreply.github.com>
    
    * Update installation.mdx
    
    Signed-off-by: dubravkoMC <159017438+dubravkoMC@users.noreply.github.com>
    
    * Update method-1.mdx
    
    Signed-off-by: dubravkoMC <159017438+dubravkoMC@users.noreply.github.com>
    
    * Update method-2.mdx
    
    Signed-off-by: dubravkoMC <159017438+dubravkoMC@users.noreply.github.com>
    
    * Update method-2.mdx
    
    Signed-off-by: dubravkoMC <159017438+dubravkoMC@users.noreply.github.com>
    
    * Update method-2.mdx
    
    Signed-off-by: dubravkoMC <159017438+dubravkoMC@users.noreply.github.com>
    
    * Update _meta.ts
    
    Signed-off-by: dubravkoMC <159017438+dubravkoMC@users.noreply.github.com>
    
    * Update method-1.mdx
    
    Signed-off-by: dubravkoMC <159017438+dubravkoMC@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: dubravkoMC <159017438+dubravkoMC@users.noreply.github.com>

--- pages/nodes/run_node/_meta.ts
+++ pages/nodes/run_node/_meta.ts
@@ -1,5 +1,7 @@
 export default {
   installation: 'Installation methods',
+  method1: 'Setting up your own Avalanchego node',
+  method2: 'Setting up an Avalanchego node via Docker',
   register: 'Register a validator',
   naas: 'NaaS'
 };
