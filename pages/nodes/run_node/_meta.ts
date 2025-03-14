293b37ed6ab5558b65cac3fc0e4cc7b002c59f85
dubravkoMC <159017438+dubravkoMC@users.noreply.github.com>
2025-03-14 15:01:23 +0100

    Update _meta.ts
    Signed-off-by: dubravkoMC <159017438+dubravkoMC@users.noreply.github.com>

--- pages/nodes/run_node/_meta.ts
+++ pages/nodes/run_node/_meta.ts
@@ -1,7 +1,7 @@
 export default {
   installation: 'Installation methods',
-  method-1: 'Setting up your own Avalanchego node',
-  method-2: 'Setting up an Avalanchego node via Docker',
+  method1: 'Setting up your own Avalanchego node',
+  method2: 'Setting up an Avalanchego node via Docker',
   register: 'Register a validator',
   naas: 'NaaS'
 };
