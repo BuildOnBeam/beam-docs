c5f08f569b9edc29bbc69f5743f992dc1365b766
Leon van der Noll <leonvdnoll@gmail.com>
2025-06-10 11:34:00 +0200

    feat: slight business ui changes

---
+++ components/ImageCard.tsx
@@ -0,0 +1,66 @@
+import { css, cx } from '@onbeam/styled-system/css';
+import { center, flex } from '@onbeam/styled-system/patterns';
+import Image from 'next/image';
+import NextLink from 'next/link';
+
+export const ImageCard = ({
+  title,
+  href,
+  img,
+}: { title: string; href: string; img: string }) => (
+  <NextLink
+    href={href}
+    className={cx(
+      'group',
+      flex({
+        direction: 'column',
+        rounded: 'md',
+        bg: 'mono.750',
+        color: 'mono.0',
+        border: '1px solid',
+        borderColor: 'mono.0/25',
+        shadow: 'md',
+        textStyle: 'md',
+      }),
+    )}
+  >
+    <div
+      className={css({
+        overflow: 'hidden',
+        aspectRatio: '396 / 378',
+        w: 'full',
+        h: 'auto',
+        roundedTop: '[inherit]',
+      })}
+    >
+      <Image
+        src={img}
+        alt=""
+        width={396}
+        height={378}
+        className={css({
+          w: 'full',
+          objectFit: 'cover',
+          transitionProperty: 'transform',
+          transitionDuration: 'normal',
+          transitionTimingFunction: 'ease-out',
+          _groupHover: {
+            transform: 'scale(1.1)',
+          },
+          _groupFocus: {
+            transform: 'scale(1.1)',
+          },
+        })}
+      />
+    </div>
+    <div
+      className={center({
+        p: '6',
+        flexGrow: 1,
+        textAlign: 'center',
+      })}
+    >
+      {title}
+    </div>
+  </NextLink>
+);
